c7130d870d0714bbc244b6c8fa870eccb57a7132
marimars2001 <marimars2001@gmail.com>
2022-05-02 12:14:46 -0700

    publish

--- Assets/Shader/DirectSpecular.hlsl
+++ Assets/Shader/DirectSpecular.hlsl
@@ -1,7 +1,7 @@
 void DirectSpecular_half(half3 Specular, half Smoothness, half3 Direction, half3 Color, half3 WorldNormal, half3 WorldView, out half3 Out)
 {
-    out = 0;
-    #ifdef SHADERGRAPH_PREVIEW
+    Out = 0;
+    #ifdef UNIVERSAL_LIGHTING_INCLUDED
         Smoothness = exp2(10 * Smoothness + 1);
         WorldNormal = normalize(WorldNormal);
         WorldView = SafeNormalize(WorldView);
